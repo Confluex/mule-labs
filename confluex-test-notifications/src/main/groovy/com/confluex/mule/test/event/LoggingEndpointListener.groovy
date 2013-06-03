ff1ed96b7e4462cae1219b94328de29d955bccec
Mike Cantrell <mike@devnull.org>
2013-06-03 14:19:21 -0500

    added logging endpoint listener

--- confluex-test-notifications/src/main/groovy/com/confluex/mule/test/event/LoggingEndpointListener.groovy
+++ confluex-test-notifications/src/main/groovy/com/confluex/mule/test/event/LoggingEndpointListener.groovy
@@ -3,15 +3,27 @@ package com.confluex.mule.test.event
 import groovy.util.logging.Slf4j
 import org.mule.api.context.notification.EndpointMessageNotificationListener
 import org.mule.context.notification.EndpointMessageNotification
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
  * Adds some simple logging around endpoint notifications to help diagnose activity and performance problems.
  */
-@Slf4j
+
 class LoggingEndpointListener implements EndpointMessageNotificationListener<EndpointMessageNotification> {
+    Logger log = LoggerFactory.getLogger(this.class)
+    String endpointName
+    Boolean logPayload = false
 
     @Override
     void onNotification(EndpointMessageNotification notification) {
-        log.debug("endpoint={} type={} id={}", notification.endpoint, notification.type, notification.source.correlationId)
+        if (!endpointName || notification.endpoint == endpointName) {
+            log.debug("endpoint={} id={} type={}", notification.endpoint,  notification.source.correlationId, notification.type,)
+            //don't invoke payloadAsString
+            if (logPayload) {
+                log.debug("endpoint={} id={} payload={}", notification.endpoint, notification.source.correlationId, notification.source.payloadAsString)
+            }
+        }
+
     }
 }
---
+++ confluex-test-notifications/src/test/groovy/com/confluex/mule/test/event/LoggingEndpointListenerTest.groovy
@@ -0,0 +1,65 @@
+package com.confluex.mule.test.event
+
+import org.junit.Before
+import org.junit.Test
+import org.mule.api.MuleMessage
+import org.mule.context.notification.EndpointMessageNotification
+import org.slf4j.Logger
+
+import static org.mockito.Mockito.*
+
+class LoggingEndpointListenerTest {
+    LoggingEndpointListener listener
+    MuleMessage message
+    EndpointMessageNotification notification
+
+
+    @Before
+    void createListener() {
+        listener = new LoggingEndpointListener(log: mock(Logger))
+        notification = mock(EndpointMessageNotification)
+        message = mock(MuleMessage)
+        when(notification.source).thenReturn(message)
+        when(message.correlationId).thenReturn("testId")
+        when(notification.endpoint).thenReturn("testEndpoint")
+        when(notification.type).thenReturn("testType")
+        when(message.payloadAsString).thenReturn("testPayload")
+    }
+
+    @Test
+    void shouldLogPayloadIfEnabled() {
+        listener.logPayload = true
+        listener.onNotification(notification)
+        verify(listener.log).debug("endpoint={} id={} type={}", "testEndpoint", "testId", "testType")
+        verify(listener.log).debug("endpoint={} id={} payload={}", "testEndpoint", "testId", "testPayload")
+    }
+
+    @Test
+    void shouldNotLogPayloadIfDisabled() {
+        listener.logPayload = false
+        listener.onNotification(notification)
+        verify(listener.log).debug("endpoint={} id={} type={}", "testEndpoint", "testId", "testType")
+        verify(listener.log, never()).debug(eq("endpoint={} id={} payload={}"), anyString(), anyString(), anyString())
+    }
+
+    @Test
+    void shouldLogIfEndpointNameIsNotConfigured() {
+        listener.endpointName = null
+        listener.onNotification(notification)
+        verify(listener.log, never()).debug(eq("endpoint={} id={} type={}"), anyString(), anyString(), anyString())
+    }
+
+    @Test
+    void shouldNotLogIfEndpointNameIsNotNotCorrect() {
+        listener.endpointName = "notTheCorrectEndpoint"
+        listener.onNotification(notification)
+        verify(listener.log, never()).debug(eq("endpoint={} id={} type={}"), anyString(), anyString(), anyString())
+    }
+
+    @Test
+    void shouldLogIfEndpointNameIsCorrect() {
+        listener.endpointName = "testEndpoint"
+        listener.onNotification(notification)
+        verify(listener.log).debug("endpoint={} id={} type={}", "testEndpoint", "testId", "testType")
+    }
+}
