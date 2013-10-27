82e99ec077e2987348d4507e3e6d0a75086c0e37
Mike Cantrell <mike@devnull.org>
2013-10-27 11:39:01 -0500

    updated mock-http tests to 0.4.2

--- confluex-functional-test/src/test/groovy/com/confluex/mule/test/functional/HttpMockFunctionalTest.groovy
+++ confluex-functional-test/src/test/groovy/com/confluex/mule/test/functional/HttpMockFunctionalTest.groovy
@@ -1,19 +1,20 @@
 package com.confluex.mule.test.functional
 
-import com.confluex.mule.test.http.MockHttpRequestHandler
-import com.confluex.mule.test.http.expectations.HeaderExpectation
-import com.confluex.mule.test.http.expectations.MediaTypeExpectation
-import com.confluex.mule.test.http.expectations.MethodExpectation
+import com.confluex.mock.http.MockHttpServer
+import com.confluex.mock.http.expectations.HeaderExpectation
+import com.confluex.mock.http.expectations.MethodExpectation
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.mortbay.jetty.Server
 import org.mule.tck.junit4.FunctionalTestCase
+
+import static javax.servlet.http.HttpServletResponse.*
+import static com.confluex.mock.http.matchers.HttpMatchers.*
+import static org.hamcrest.Matchers.*
 
 class HttpMockFunctionalTest extends FunctionalTestCase {
 
-    Server server
-    MockHttpRequestHandler handler
+    MockHttpServer server
 
     /**
      * The Mule Configuration file(s) to load when starting the embedded Mule server
@@ -28,11 +29,7 @@ class HttpMockFunctionalTest extends FunctionalTestCase {
      */
     @Before
     void createMockHttpServer() {
-        server = new Server(9001)
-        handler = new MockHttpRequestHandler()
-        server.handler = handler
-        server.start()
-        server.stopAtShutdown = true
+        server = new MockHttpServer(9001)
     }
 
     /**
@@ -49,14 +46,15 @@ class HttpMockFunctionalTest extends FunctionalTestCase {
      */
     @Test
     void shouldListCatalog() {
-        handler.when("/catalog")
-                .thenReturnResource("/payloads/catalog.xml")
+        server.respondTo(get("/catalog"))
+                .withResource("/payloads/catalog.xml")
                 .withStatus(200)
 
         def message = muleContext.client.send("requestCatalog", "", [:])
         assert message.payloadAsString == this.class.getResourceAsStream("/payloads/catalog.xml").text
 
-        handler.verify("/catalog", MethodExpectation.GET)
+        def requests = server.requests?.findAll { it.method == "GET" && it.path == "/payloads/catalog.xml" }
+        assert requests.size() == 1
     }
 
     /**
@@ -64,8 +62,8 @@ class HttpMockFunctionalTest extends FunctionalTestCase {
      */
     @Test
     void shouldUpdateCatalog() {
-        handler.when("/catalog")
-                .thenReturnText("Updated")
+        server.respondTo(put("/catalog"))
+                .withBody("Updated")
                 .withStatus(302)
                 .withHeader("Location", "http://localhost:9001/catalog")
 
@@ -78,17 +76,11 @@ class HttpMockFunctionalTest extends FunctionalTestCase {
 
         // vm endpoint is async, we need to wait until the handler processes
         // the request or times out (error condition)
-        assert handler.waitForEvents(1, 10000)
+        assert server.waitFor(put("/catalog"), 1, 10000)
 
-        // now we can do verifications
-        handler.verify("/catalog",
-                MethodExpectation.PUT,
-                MediaTypeExpectation.XML,
-                new HeaderExpectation("updatedBy", "Bill Murray")
-        )
 
         // you can also get access to the raw client request data if desired
-        def requests = handler.getRequests("/catalog")
+        def requests = server.requests.findAll { it.path == "/catalog" && it.method == "PUT"}
         assert requests.size() == 1
         assert requests[0].headers['Content-Type'] == "application/xml"
         assert requests[0].headers['X-MULE_ENDPOINT'] == "http://localhost:9001/catalog"
